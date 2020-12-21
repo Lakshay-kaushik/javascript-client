c1657ee59c3d7c696011e321140e516dd982d4ef
Lakshay-kaushik <lakshay.kaushik@successive.tech>
2020-12-21 12:30:41 +0545

    Day 4

--- src/components/TextField/TextField.jsx
+++ src/components/TextField/TextField.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Input, Error } from './style';
-
 const TextField = (props) => {
   const { disabled, value, error } = props;
   if (Error) {
     return (
       <>
-        <Input type="text" value={value} disabled={disabled} error />
+        <Input type="text" value={value} disabled={props.disabled} error />
         <br />
         <Error>{ error }</Error>
       </>
     );
   }
   return (
-    <Input type="text" value={value} disabled={disabled} />
+    <Input type="text" value={value} disabled={props.disabled} />
   );
 };
 TextField.propTypes = {
