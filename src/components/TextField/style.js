ead21703ef3f1d3bfbfdf4d2c105a0e23f45644b
Lakshay-kaushik <lakshay.kaushik@successive.tech>
2020-12-23 17:21:13 +0545

    day 6

--- src/components/TextField/style.js
+++ src/components/TextField/style.js
@@ -10,13 +10,14 @@ width: 100%;
 font-size: 18px;
 font-weight: bold;
 color: solid darkgray;
+`;
 
 const Input = styled.input`
 width: 94%;
 padding: 10px 15px;
 border: 1px solid orange;
 border-radius: 5px;
-color: solid darkgray;
+color: solid darkgray ;
 ${(props) => props.error
 && css`
 border: 1px solid black;
